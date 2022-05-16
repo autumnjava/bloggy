e896b195107a3362fa6893ec55fe6c3d7a3da544
autumnjava <creizy@gmail.com>
2022-05-16 10:31:43 +0200

    initial commit v3

---
+++ v3/client/src/CommentCreate.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+function CommentCreate({ postId }) {
+    const [content, setContent] = useState('');
+
+    const onSubmitFn = async (e) => {
+        e.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        });
+        setContent('');
+    }
+
+  return (
+    <div style={{margin: '5px 10px'}}>
+        <form onSubmit={onSubmitFn} >
+            <div className="form-group"  style={{padding: '20px 0'}}>
+                <label>Title</label>
+                <input className="form-control" value={content} onChange={e => setContent(e.target.value)} />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default CommentCreate
---
+++ v3/client/src/CommentList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CommentList({ comments }) {
+
+  return (
+    <ul>
+      {comments.map(comment => 
+        <li key={comment.id}>{comment.content}</li> 
+      )}
+    </ul>
+  )
+}
+
+export default CommentList
