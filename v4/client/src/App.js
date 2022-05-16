1340fe8a29a08a61502997c18acd683a43e6a640
autumnjava <creizy@gmail.com>
2022-05-16 10:45:49 +0200

    v4 initial commit

---
+++ v4/client/src/App.js
@@ -0,0 +1,16 @@
+import PostCreate from "./PostCreate"
+import PostList from "./PostList"
+
+
+function App() {
+  return (
+    <div className="container ">
+    <h1>Create post!</h1>
+    <PostCreate />
+    <hr />
+    <PostList />
+  </div>
+  );
+}
+
+export default App;
---
+++ v4/client/src/PostCreate.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+function PostCreate() {
+    const [title, setTitle] = useState('');
+
+    const onSubmitFn = async (e) => {
+        e.preventDefault();
+        await axios.post('/posts/create', {
+            title
+        });
+        setTitle('');
+    }
+  return (
+    <div>
+        <form onSubmit={onSubmitFn} >
+            <div className="form-group"  style={{padding: '20px 0'}}>
+                <label>Title</label>
+                <input className="form-control" value={title} onChange={e => setTitle(e.target.value)} />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default PostCreate
